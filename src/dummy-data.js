0835fcf0225a6fc14e559054d3445f783cb0a7f0
mikayla-bryant <mikaylabryant25@gmail.com>
2020-12-08 17:35:03 -0500

    Added liked functionality

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -26,7 +26,7 @@ const dummyData = [
     },
     {
         id: 2,
-        username: "twitch",
+        username: "funtimes",
         thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
         imageUrl: 'https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80',
         likes: 4307,
